a8776467db87edec5a2d787b475e98655b4be252
thiviergejonathan <thiviergejonathan@gmail.com>
2023-09-07 18:41:04 -0400

    test bug

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v16';
+const CACHE_NAME = 'static-cache-v17';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
