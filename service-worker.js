35edfce8cb4481edf2e4b2bef6e2bb9e0eac9a7a
thiviergejonathan <thiviergejonathan@gmail.com>
2023-09-07 15:56:38 -0400

    cache et connexion

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -17,12 +17,35 @@ self.addEventListener('install', (evt) => {
     );
     self.skipWaiting();
 });
+
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
     //Remove previous cached data from disk.
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
     self.clients.claim();
 });
+
 self.addEventListener('fetch', (evt) => {
     console.log('[ServiceWorker] Fetch', evt.request.url);
     //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(() => {
+            return caches.open(CACHE_NAME).then((cache) => {
+                return cache.match('/Cochenille/PointNClick/offline.html');
+            });
+        })
+    );
 });
